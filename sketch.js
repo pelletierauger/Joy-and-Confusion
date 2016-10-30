e2fdab7a54f5c07c89e639b702aad3c5b728fd4d
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-10-30 03:37:56 -0400

    Update the p5.tools submodule

--- sketch.js
+++ sketch.js
@@ -13,6 +13,7 @@ var norma = false;
 var globalValues = {};
 var song;
 var repositionSong = false;
+var sumSheet;
 
 function setup() {
     song = loadSound("mindonfire.mp3", playSong);
@@ -20,7 +21,8 @@ function setup() {
     canvas = createCanvas(windowWidth, windowWidth * 9 / 16);
     ctx = canvas.drawingContext;
     frameRate(20);
-    createInterface();
+    sumSheet = sumXSheet(xSheet);
+    createInterface(0, sumSheet, 0);
     configureInterface();
     // createInfoDiv();
     // setupInfoDiv();
@@ -31,10 +33,35 @@ function setup() {
 }
 
 function configureInterface() {
+
+    folders.particles = new Folder("Particles", true);
+    buttons.resetW = new Button("Reset white dot position and velocity", folders.particles.div, function() {});
+    // resetW = createButton('Reset white dot position and velocity');
+    // resetW.parent(infoDiv);
+    // resetA = createButton('Reset all');
+    buttons.resetA = new Button("Reset all", folders.particles.div, function() {});
+    buttons.resetA.button.style("float", "right");
+    //-------- Curve selector------------------//
+    menus.curves = new Menu("Path", folders.particles.div);
+    for (var c = 0; c < curves.length; c++) {
+        menus.curves.menu.option(curves[c].name);
+    }
+    menus.curves.menu.changed(function() {
+        var item = menus.curves.menu.value();
+        for (var i = 0; i <  curves.length; i++) {
+            if (item === curves[i].name) {
+                shape = curves[i];
+            }
+        }
+    });
+    menus.curves.menu.value(curves[2].name);
+    shape = curves[2];
+    sliders.accMult = new Slider("White dot Acceleration rate", 0, 1, 1, 0.001, folders.particles.div);
+    sliders.velMult = new Slider("White dot Friction", 0.999, 1, 0.999, 0.00001, folders.particles.div);
+    sliders.speed = new Slider("Yellow Dot Rotation rate", 0, 1, 0.29, 0.01, folders.particles.div);
+
+
     folders.spiral = new Folder("Spirals", true);
-    sliders.accMult = new Slider("White dot Acceleration rate", 0, 1, 1, 0.001, folders.spiral.div);
-    sliders.velMult = new Slider("White dot Friction", 0.999, 1, 0.999, 0.00001, folders.spiral.div);
-    sliders.speed = new Slider("Yellow Dot Rotation rate", 0, 1, 0.29, 0.01, folders.spiral.div);
     sliders.zoom = new Slider("Canvas scale", 0, 20, 1, 0.01, folders.spiral.div);
     folders.superformula = new Folder("Superformula", true);
     sliders.n2 = new Slider("n2", 0, 10, 1, 0.1, folders.superformula.div);
@@ -46,6 +73,11 @@ function configureInterface() {
     sliders.particleScalar = new Slider("Particle scalar", 0, 200, 1, 0.001, folders.spiral.div);
     sliders.spiralScalar = new Slider("Spiral scalar", 1, 200, 1, 0.01, folders.spiral.div);
     sliders.levels = new Slider("Levels", -200, 100, 0, 1, folders.spiral.div);
+
+    sliders.timeline.max = sumSheet;
+    sliders.timeline.min = 0;
+    sliders.timeline.start = 0;
+    // sumSheet, drawCount
 }
 
 function playSong() {
@@ -90,7 +122,7 @@ function draw() {
         repositionSong = false;
     }
     sliders.timeline.set(drawCount);
-    info1.html(queryXSheet(xSheet));
+    sliders.timeline.paragraph.html(queryXSheet(xSheet) + ", drawCount : " + drawCount);
 }
 
 function printBackgroundGradient() {
@@ -148,10 +180,12 @@ function keyPressed() {
     if (key == 'g' || key == 'G') {
         if (showPanel) {
             showPanel = false;
-            infoDiv.style("display", "none");
+            interface.style("display", "none");
+            timeline.style("display", "none");
         } else {
             showPanel = true;
-            infoDiv.style("display", "block");
+            interface.style("display", "block");
+            timeline.style("display", "block");
         }
     }
 }
